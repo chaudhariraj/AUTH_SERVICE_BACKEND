cb84add38f8d097f8d672b00e78f0e6d3764e63d
Raj Chaudhari <craj0093@gmail.com>
2024-02-09 11:02:36 +0530

    basic express

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
-
-
 import app from "./app";
 import { Config } from "./config";
 
 const startServer = () => {
-try {
-    const port = Config.PORT
+  try {
+    const port = Config.PORT;
     // eslint-disable-next-line no-console
-    app.listen(port, () => console.log(`Listeing on port ${port}`))
-    
-} catch (error) {
+    app.listen(port, () => console.log(`Listeing on port ${port}`));
+  } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error);
     process.exit(1);
-    
-}
-}
+  }
+};
 
 startServer();
