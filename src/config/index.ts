385c90d75739c8837fb5bd31a5ede58c4daaf9c0
raj chaudhari <craj0093@gmail.com>
2024-07-07 17:21:52 +0530

    setup database (type ORM)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,16 @@
 import { config } from "dotenv";
+import path from 'path';
 
-config();
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_USERNAME, DB_PORT, DB_PASSWORD, DB_NAME } = process.env;
 
 export const Config = {
   PORT,
-  NODE_ENV
-};
+  NODE_ENV,
+  DB_HOST, 
+  DB_USERNAME, 
+  DB_PORT, 
+  DB_PASSWORD, 
+  DB_NAME
+};  
